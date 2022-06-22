44c903d7c80d7eb1febdadb0594783df7615489e
yzbai <yzbai@brlite.com>
2022-06-22 22:36:11 +0800

    Resolve the issue.

--- Program.cs
+++ Program.cs
@@ -8,12 +8,7 @@ namespace WebApplication1
         public static void Main(string[] args)
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-
             
-
-            
-            builder.WebHost.UseStartup<Startup>();
 
 
             // Add services to the container.
@@ -33,6 +28,10 @@ namespace WebApplication1
 
             app.UseHttpMethodOverride();
 
+            //模板代码默认不添加UseRouting，默认吧Routing加载Pipeline的最开端，这样就是先路由，再改HttpMethod，没用。
+            //所以一定要显式的在这里添加。
+            app.UseRouting();
+
             app.UseAuthorization();
 
 
@@ -51,19 +50,21 @@ namespace WebApplication1
             {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
+
             app.UseHttpMethodOverride();
 
-            //app.Run(async (context) =>
-            //{
-            //    foreach (var header in context.Request.Headers)
-            //    {
-            //        await context.Response.WriteAsync($"{header.Key}: {header.Value}\r\n");
-            //    }
-            //    await context.Response.WriteAsync($"Method: {context.Request.Method}\r\n");
-            //    await context.Response.WriteAsync($"Scheme: {context.Request.Scheme}\r\n");
-            //    await context.Response.WriteAsync($"RemoteIP: {context.Connection.RemoteIpAddress}\r\n");
-            //    await context.Response.WriteAsync($"RemotePort: {context.Connection.RemotePort}\r\n");
-            //});
+
+            app.Run(async (context) =>
+            {
+                foreach (var header in context.Request.Headers)
+                {
+                    await context.Response.WriteAsync($"{header.Key}: {header.Value}\r\n");
+                }
+                await context.Response.WriteAsync($"Method: {context.Request.Method}\r\n");
+                await context.Response.WriteAsync($"Scheme: {context.Request.Scheme}\r\n");
+                await context.Response.WriteAsync($"RemoteIP: {context.Connection.RemoteIpAddress}\r\n");
+                await context.Response.WriteAsync($"RemotePort: {context.Connection.RemotePort}\r\n");
+            });
         }
 
         // Entry point for the application.
